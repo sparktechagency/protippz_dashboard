f7610dad3a7688bd4b91bfa7249399230ae8c5cc
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-12-15 12:42:54 +0600

    s

--- src/Utils/BaseUrl.js
+++ src/Utils/BaseUrl.js
@@ -1,2 +1,2 @@
 // export const url = 'http://192.168.10.11:5050'
-export const url = "http://3.135.129.190:5000"
+export const url = "http://3.15.72.3:5000"
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: '0.0.0.0', 
-    host: '3.135.129.190', 
+    host: '0.0.0.0', 
+    // host: '3.135.129.190', 
     port: 3000, 
   }
 })
