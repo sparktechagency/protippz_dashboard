b597257aa0fc78249ba83aacd9fbe33cb106ee5f
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-12-15 15:06:53 +0600

    s

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', 
+    host: '3.15.72.3',
     // host: '3.135.129.190', 
     port: 3000, 
   }
