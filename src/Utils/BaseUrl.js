21e858d72c7c180c1873be02e9cb5fec2740e80c
hosain ali <hosainahmed534745@gmail.com>
2025-04-03 11:50:11 +0600

    update baseurl

--- src/Utils/BaseUrl.js
+++ src/Utils/BaseUrl.js
@@ -1,8 +1,8 @@
 // export const url = 'http://192.168.10.11:5050'
 // export const url = "http://18.218.23.153:5000"
 import img from '../../src/assets/icons/PT Logo.png';
-export const url = 'http://10.0.60.137:5050';
-// export const url = 'https://api.protippz.com';
+// export const url = 'http://10.0.60.137:5050';
+export const url = 'https://api.protippz.com';
 
 export const imageUrl = (image) => {
   return image
